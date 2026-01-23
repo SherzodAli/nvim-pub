66b4e1f9f9b524feb9d913ad0a6efbccb4ad98c7
SherzodAli <sherzodalimov2002@gmail.com>
2026-01-23 10:27:57 +0500

    change theme

--- lua/conf/plugins/theme.lua
+++ lua/conf/plugins/theme.lua
@@ -1,17 +1,16 @@
 return {
 	{
-		"folke/tokyonight.nvim",
+		"ellisonleao/gruvbox.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		config = function()
 			---@diagnostic disable-next-line: missing-fields
-			require("tokyonight").setup({
+			require("gruvbox").setup({
 				styles = {
 					comments = { italic = false },
 				},
 			})
 
-			-- 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.colorscheme("gruvbox")
 		end,
 	},
 }
