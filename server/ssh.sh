62f0f15bed91aa72bf62aefe16ca19922f9fb202
SherzodAli <sherzodalimov2002@gmail.com>
2026-03-06 15:56:42 +0500

    add info on how to disable password login

--- server/ssh.sh
+++ server/ssh.sh
@@ -5,4 +5,11 @@ touch ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
 # echo 'ssh-ed25519 AAAA...your-public-key...' >> ~/.ssh/authorized_keys
-# /etc/ssh/sshd_config.d/ -> PermitRootLogin no -> sudo sshd -t -> sudo systemctl reload sshd/ssh
+
+# sudo nano /etc/ssh/sshd_config (/etc/ssh/sshd_config.d/* - check if not overwritten)
+# sudo sshd -t -> sudo systemctl reload sshd/ssh
+PubkeyAuthentication yes
+PasswordAuthentication no
+KbdInteractiveAuthentication no
+ChallengeResponseAuthentication no
+UsePAM yes
